77ad986aa69886a9b3f6d155a3e31c1a1266810f
Felipe Pena <felipe@php.net>
2016-07-30 21:51:44 -0300

    - unit test

--- test.py
+++ test.py
@@ -1,47 +1,47 @@
-from bipbop.client import Database
-from bipbop.client import NameByCPFCNPJ
+import unittest
+import xml.etree.ElementTree as ET
 from bipbop.client import WebService
 from bipbop.client import ServiceDiscovery
 
-import xml.etree.ElementTree as ET
+class BipbopTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.ws = WebService('907703004bbdd0a7f11e0398b5f200ac')
+        cls.sd = ServiceDiscovery.factory(cls.ws)
 
-ws = WebService('907703004bbdd0a7f11e0398b5f200ac')
+    def test_basicWebservice(self):
+        xml = self.ws.post("SELECT FROM 'PLACA'.'CONSULTA'", {'placa': 'OGD1557'})
+        self.assertFalse(xml is None)
 
-"""
-xml = ws.post("SELECT FROM 'PLACA'.'CONSULTA'", {'placa': 'OGD1557'})
-if not xml is None:
-    print ET.tostring(xml.getroot())
-    xml.write('output.xml')
-"""
+    def test_listDatabase(self):        
+        self.assertTrue(len(list(self.sd.list_databases())) > 0)
 
-sd = ServiceDiscovery.factory(ws)
+    def test_getDbName(self):
+        db = self.sd.get_database('CORREIOS')
+        self.assertEqual('CORREIOS', db.name())
 
-"""
-for db in sd.list_databases():
-    odb = sd.get_database(db.get('name'))
-    print 'DB: ' + odb.name() + ' - ' + odb.get('description')
-    for table in odb.list_tables():
-        otb = odb.get_table(table.get('name'))
-        print '- Table: ' + table.get('name')
-        for field in otb.get_fields():
-            print '\t- Field: ' + field.name() 
-            #print field.options()
-            print field.group_options()
+    def test_getTableName(self):
+        db = self.sd.get_database('CORREIOS')
+        table = db.get_table('CONSULTA')
+        self.assertEqual('CONSULTA', table.name())
 
-    print 
-"""
+    def test_getFieldName(self):
+        db = self.sd.get_database('CORREIOS')
+        table = db.get_table('CONSULTA')
+        self.assertEqual('cep', list(table.get_fields())[0].name())
 
-db = sd.get_database('CORREIOS')
+    def test_traverseDb(self):
+        for db in self.sd.list_databases():
+            self.assertIsNotNone(db.get('name'))
+            odb = self.sd.get_database(db.get('name'))
+            self.assertIsNotNone(odb)            
+            for table in odb.list_tables():
+                self.assertIsNotNone(table.get('name'))
+                otb = odb.get_table(table.get('name'))
+                self.assertIsNotNone(otb)                
+                for field in otb.get_fields():
+                    self.assertIsNotNone(field.name())
 
-print db.name()
-for table in db.list_tables():
-    print table
-
-table = db.get_table('CONSULTA')
-
-print table.name()
-
-for fields in table.get_fields():
-    print fields.name()
-    print fields.options()
-
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(BipbopTest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
