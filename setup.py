93c4fa64c93bd2e03aefa53c9da551a965db1356
Lucas Fernando Amorim <lf.amorim@bipbop.com.br>
2016-07-31 15:42:14 -0300

    pacote pypi

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='bipbop',
+        version='0.1',
+        description='Official library for BIPBOP API',
+        author='Felipe Pena',
+        author_email='tech@bipbop.com.br',
+        url='https://github.com/bipbop/bipbop-python',
+        packages=['bipbop'])
